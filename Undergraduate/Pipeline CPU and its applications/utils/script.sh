04cf767c72224720d765f98aa9fab68b7e718756
YU-CHI CHU <tfb.zhuyuqi@gmail.com>
2025-01-21 21:12:11 +0000

    Delete Undergraduate/Pipeline CPU and its applications directory

--- Undergraduate/Pipeline CPU and its applications/utils/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-riscv32-unknown-elf-gcc -march=rv32i -mabi=ilp32 -c $1.c -o $1.o
-riscv32-unknown-elf-ld -T link.ld init.o $1.o -o main.elf
-riscv32-unknown-elf-objcopy -O verilog main.elf main.hex
-exit 0
